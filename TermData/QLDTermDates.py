f8521aaa510629e09fd8aec10c367eebf4f8708c
Brandon 'Wedge' White <koggars@gmail.com>
2024-03-26 22:17:07 +1000

    Refactor QLDTermDates class and parsing methods
    This commit overhauls the QLDTermDates class and the related parsing methods. It reworks and improves how term date data is parsed from the Queensland term dates webpage, providing cleaner and more accurate term data extraction. It also updates the TermData __init__.py for the changes in QLDTermDates.
    
    Signed-off-by: Brandon 'Wedge' White <koggars@gmail.com>

--- TermData/QLDTermDates.py
+++ TermData/QLDTermDates.py
@@ -1,30 +1,14 @@
+import json
 from typing import Dict
 
 import requests
 from datetime import datetime
 from collections import defaultdict
-from ics import Calendar
-import json
 from bs4 import BeautifulSoup
+import re
 
 URL = "https://education.qld.gov.au/about-us/calendar/term-dates"
-
-
-def parse_paragraph(text, year):
-    text = text.split('—')[0]  # remove the part that is not needed
-    start_date_string, end_date_string = text.split(
-        'to')  # split start and end dates
-    start_date_string = start_date_string.split(
-        ':')[1].strip()  # remove 'Term 1:' part
-    start_date_string += f" {year}"
-    end_date_string += f" {year}"
-    start_date = datetime.strptime(
-        start_date_string, '%A %d %B %Y')  # convert string to date
-    end_date = datetime.strptime(
-        end_date_string.strip(), '%A %d %B %Y')  # convert string to date
-
-    return {'start': start_date.strftime('%Y-%m-%d'), 'end': end_date.strftime('%Y-%m-%d')}
-
+FUTURE_URL = "https://education.qld.gov.au/about-us/calendar/future-dates"
 
 class QLDTermDates:
 
@@ -34,29 +18,37 @@ class QLDTermDates:
         self.download_data()
 
     @staticmethod
-    def get_quarter(dt):
-        return (dt.month - 1) // 3 + 1
+    def parse_paragraph(text, year):
+        text = text.split('—')[0]  # remove the part that is not needed
+        start_date_string, end_date_string = text.split(
+            'to')  # split start and end dates
+        start_date_string = start_date_string.split(
+            ':')[1].strip()  # remove 'Term 1:' part
+        start_date_string += f" {year}"
+        end_date_string += f" {year}"
+        start_date = datetime.strptime(
+            start_date_string, '%A %d %B %Y')  # convert string to date
+        end_date = datetime.strptime(
+            end_date_string.strip(), '%A %d %B %Y')  # convert string to date
 
-    @staticmethod
-    def parse_date(date_str):
-        try:
-            # str.strip() will remove lead/trail white spaces
-            return datetime.strptime(date_str.strip(), '%Y-%m-%d')
-        except ValueError:
-            print(f"Unable to parse date: {date_str}")
-            return None
+        return {'start': start_date.strftime('%Y-%m-%d'), 'end': end_date.strftime('%Y-%m-%d')}
+
+    def parseCurent(self, response):
+        soup = BeautifulSoup(response.text, 'html.parser')
+        year = soup.find('h3', string="Queensland term dates").find_previous("h2").text.strip()
+        h3 = soup.find('h3', string="Queensland term dates")
+        # find the next 4 strong elements containing 'Term'
+        terms = h3.find_all_next('strong', string=re.compile('^Term'))[:4]
+        for term in terms:
+            term_text = term.text.strip()
+            dates = self.parse_paragraph(term.parent.text, year)
+            self.data_dict[year][term_text] = dates
 
     def download_data(self):
-
         response = requests.get(self.url)
-
         if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            year = soup.find('h3', string="Queensland term dates").find_previous("h2").text.strip()
-            terms = ['Term 1', 'Term 2', 'Term 3', 'Term 4']
-            for term in terms:
-                raw_data = soup.find('strong', string=term)
-                if raw_data:
-                    dates = parse_paragraph(raw_data.parent.text, year)
-                    self.data_dict[year][term] = dates
+            self.parseCurent(response)
         return self.data_dict
+
+test = QLDTermDates()
+print(json.dumps(test.data_dict, indent=2))
