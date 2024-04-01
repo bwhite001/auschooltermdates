c3f28a9818a396103cb4e2fce6b96b972fc8a9b6
Brandon 'Wedge' White <koggars@gmail.com>
2024-04-01 21:50:09 +1000

    Refactor code and change GitHub workflow trigger
    The main.py code has been refactored to make it clear and concise by removing verbose comments and unnecessary details. A check and creation of 'dist' directory has been added before writing the JSON file. Additionally, the trigger for the GitHub workflow has been altered from tags to the 'main' branch.
    
    Signed-off-by: Brandon 'Wedge' White <koggars@gmail.com>

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
 import json
 import datetime
+import os
 from TermData import *
 from TermData.SATermDates import SATermDates
 from TermData.TASTermDates import TASTermDates
 
-
 class DateTimeEncoder(json.JSONEncoder):
-    """JSON encoder class for encoding datetime objects.
-
-        This class inherits from the `json.JSONEncoder` class and provides custom encoding for datetime objects. When encoding a datetime object, it converts it to its ISO 8601 string representation
-    *.
-
-        Args:
-            json.JSONEncoder: The base class for JSON encoders.
-
-        Methods:
-            default: Overrides the default method from the base class to provide custom encoding for datetime objects.
-
-        Example:
-            import json
-            import datetime
-
-            class DateTimeEncoder(json.JSONEncoder):
-                def default(self, o):
-                        if isinstance(o, datetime.datetime):
-                            return o.isoformat()
-
-                        return super().default(o)
-        """
-
     def default(self, o):
         if isinstance(o, datetime.datetime):
             return o.isoformat()
@@ -37,15 +14,8 @@ class DateTimeEncoder(json.JSONEncoder):
 
 
 def run():
-    """
-    Collects and writes the term dates for all states to a JSON file.
-
-    :return: None
-    """
-    # Term dates for all states
     all_term_dates = {}
 
-    # Loop through all states
     states = {
         'ACT': ACTTermDates(),
         'NSW': NSWTermDates(),
@@ -58,12 +28,13 @@ def run():
     }
 
     for state, term_dates in states.items():
-        # Get the term dates for all years
         if term_dates.data_dict is not None:
             state_term_dates = term_dates.data_dict
             all_term_dates[state] = state_term_dates
 
-    # Write the term dates for all states to a JSON file
+    if not os.path.exists("dist"):
+        os.makedirs("dist")
+
     with open("dist/term_dates.json", "w") as f:
         json.dump(all_term_dates, f, ensure_ascii=False, indent=4, cls=DateTimeEncoder)
 
